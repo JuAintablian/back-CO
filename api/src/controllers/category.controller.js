2af1d471bb88d16128a6ace7d4cb1133c1ecf27f
Juuh Aintablian <juuh.ian@gmail.com>
2021-02-16 21:11:10 -0300

    rotas e funcoes categoria

--- api/src/controllers/category.controller.js
+++ api/src/controllers/category.controller.js
@@ -1,17 +1,69 @@
 const db = require("../config/database");
 
+
+// ==> Método responsável por obter a lista de categorias:
+
+exports.getCategoryList = async (req, res) => {
+  const response = await db.query('SELECT * FROM category ORDER BY id ASC');
+
+  res.status(200).send(response.rows);
+};
+
+// ==> Método responsável por obter uma categoria por Id:
+
+exports.getCategoryById = async (req, res) => {
+  const categoryId = parseInt(req.params.id);
+
+  const response = await db.query('SELECT * FROM category WHERE id = $1', [categoryId]);
+  res.status(200).send(response.rows);
+};
+
 // ==> Método responsável por criar uma nova 'Categoria':
 
 exports.createCategory = async (req, res) => {
   const { name } = req.body;
   const { rows } = await db.query(
-    "select * from category"
+    "INSERT INTO category (name) VALUES ($1)",
+    [name]
   );
 
   res.status(201).send({
     message: "Category added successfully!",
     body: {
-       rows
+      category: name
     },
   });
+};
+
+// ==> Método responsável por atualizar uma 'Categoria' pelo 'Id':
+
+exports.updateCategory = async (req, res) => {
+  const categoryId = parseInt(req.params.id);
+  const { name } = req.body;
+
+  const response = await db.query(
+    "UPDATE category SET name = $1 WHERE id = $2",
+    [name, categoryId]
+  );
+
+  res.status(200).send({ message: "Category Updated Successfully!" });
+};
+
+// ==> Método responsável por excluir uma 'Categoria' pelo 'Id':
+
+exports.deleteCategory = async (req, res) => {
+  const categoryId = parseInt(req.params.id);
+  
+  const hasCategory = await db.query('SELECT * FROM category WHERE id = $1', [categoryId]);
+
+  if(hasCategory.rows.length < 1) {
+    return res.status(500).send({ message: 'Não foi encontrada nenhuma categoria', categoryId });
+  } else {
+    await db.query('DELETE FROM category WHERE id = $1', [
+      categoryId
+    ]);
+  
+    res.status(200).send({ message: 'Product deleted successfully!', categoryId });
+  }
+  
 };
--- api/src/routes/category.routes.js
+++ api/src/routes/category.routes.js
@@ -1,10 +1,10 @@
 const router = require('express-promise-router')();
 const categoryController = require('../controllers/category.controller');
 
-router.post('/category', categoryController.createCategory);
 router.get('/category', categoryController.getCategoryList);
 router.get('/category/:id', categoryController.getCategoryById);
-router.delete('/category/:id', categoryController.deleteCategory);
+router.post('/category', categoryController.createCategory);
 router.patch('/category/:id', categoryController.updateCategory);
+router.delete('/category/:id', categoryController.deleteCategory);
 
 module.exports = router;
